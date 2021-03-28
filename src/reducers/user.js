75d082dfd9e03585539a489aecb07a26e3f83f99
fabrizio <faber.rvn@gmail.com>
2021-03-28 17:52:54 +0200

    #16 Refactor Login

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,16 +1,11 @@
 import { AUTH, LOGOUT } from '../actions/types';
-import { saveItem } from '../helpers/storage';
-
-const USER = 'USER';
 
 const userReducer = (state = null, action) => {
   switch (action.type) {
     case AUTH:
-      saveItem(USER, { ...action.payload });
       return { ...state, ...action.payload };
 
     case LOGOUT:
-      localStorage.removeItem(USER);
       return null;
 
     default:
